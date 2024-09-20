1c44b66cd4103882881d48edbb599b9742f6a614
omer-sh <67651910+omer-sh@users.noreply.github.com>
2024-09-20 23:07:43 +0300

    without debug

--- server.py
+++ server.py
@@ -185,7 +185,7 @@ def update_user():
         return jsonify({"error": "Unsupported Media Type"}), 415
 
 def app(environ, start_response):
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
 
 if __name__ == '__main__':
     app.run()
